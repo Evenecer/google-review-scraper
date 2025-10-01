db376f20b7f904650963823a2cd675fe5d1d502c
Petey <46226573+Petey1337@users.noreply.github.com>
2025-10-01 15:00:53 -0400

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist node_modules (
+  echo Installing dependencies...
+  call npm install || goto :install_fail
+)
+cls
+echo Starting app...
+node index.js
+
+echo.
+echo (App exited with code %errorlevel%)
+pause
+exit /b
+
+:install_fail
+echo Install failed.
+pause
+exit /b 1
